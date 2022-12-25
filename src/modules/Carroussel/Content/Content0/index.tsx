9d98fc38bdb5ecde0f033a207abce14a91313f82
pedrogilsenarego <pedrogilsenarego@gmail.com>
2022-12-25 01:25:12 +0000

    a

--- src/modules/Carroussel/Content/Content0/index.tsx
+++ src/modules/Carroussel/Content/Content0/index.tsx
@@ -1,18 +1,37 @@
-import { Box, Typography, Grid } from "@mui/material"
+import { Box, Typography, Grid } from "@mui/material";
 
 const Content0 = () => {
-
   return (
     <Box style={{ margin: "0px 10% 0px 10%" }}>
-      <Grid container xs={6}>
+      <Grid container xs={6} style={{ marginTop: "10%" }}>
         <Grid item>
-          <Typography fontSize="1.5em" style={{ fontWeight: 800 }}>
+          <Box
+            style={{
+              backgroundImage:
+                "linear-gradient(to right, rgb(221, 5, 221), rgb(16, 4, 102))",
+              fontSize: "0.5em",
+              fontWeight: 700,
+              padding: "0.7em",
+              borderRadius: "3px",
+              width: "fit"
+            }}
+          >
+            Something DOnt know what
+          </Box>
+          <Typography fontSize='1.5em' style={{ fontWeight: 800 }}>
             Hi!! I'm Pedro, a Web Dev
+          </Typography>
+          <Typography fontSize='0.5em'>
+            Started my career as a chemical engineer, and evetually fall in love
+            with everything with web. Started my career as a chemical engineer,
+            and evetually fall in love with everything with web. Started my
+            career as a chemical engineer, and evetually fall in love with
+            everything with web.
           </Typography>
         </Grid>
       </Grid>
     </Box>
-  )
-}
+  );
+};
 
-export default Content0
+export default Content0;
