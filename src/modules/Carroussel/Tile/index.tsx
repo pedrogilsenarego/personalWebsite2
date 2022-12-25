e3945c688b50927d4c45e20aa9696ed651d0e469
pedrogilsenarego <pedrogilsenarego@gmail.com>
2022-12-25 14:56:45 +0000

    a

--- src/modules/Carroussel/Tile/index.tsx
+++ src/modules/Carroussel/Tile/index.tsx
@@ -1,24 +1,26 @@
 import { Box } from "@mui/material";
 
-
 interface Props {
   tileIndex: number;
   index: number;
-  position: string
-  setTileIndex: (tileIndex: number) => void
-  content?: JSX.Element
+  position: string;
+  setTileIndex: (tileIndex: number) => void;
+  content?: JSX.Element;
 }
 
 const Tile = ({ tileIndex, index, position, setTileIndex, content }: Props) => {
-
   const left = () => {
-    if (position === "front") return 0
-    if (position === "left") return -1100
-    if (position === "right") return 1100
-    else return 0
-  }
-
-
+    if (position === "front") return 0;
+    if (position === "left") return "-32%";
+    if (position === "right") return "32%";
+    else return 0;
+  };
+  const right = () => {
+    if (position === "front") return 0;
+    if (position === "left") return "32%";
+    if (position === "right") return "-32%";
+    else return 0;
+  };
 
   return (
     <Box
@@ -31,19 +33,27 @@ const Tile = ({ tileIndex, index, position, setTileIndex, content }: Props) => {
         marginTop: "auto",
         marginBottom: "auto",
         left: left(),
-        right: 0,
+        right: right(),
         marginLeft: "auto",
         marginRight: "auto",
         zIndex: position === "front" ? 1000 : 1,
         height: "85vh",
         width: "85vh",
-        backgroundColor: "#0E0A09",
+        backgroundColor: tileIndex === 1 ? "#0E0A0966" : "#0E0A09",
         borderRadius: "12%",
         transition: "all 2s ease",
         boxShadow: "5px 5px 5px #00000066",
-        transform: position === "front" ? "scale(1)" : "scale(0.65)"
+        transform:
+          position === "front" && tileIndex === 1
+            ? "scale(0.85) perspective(800px) rotateX(55deg)"
+            : position === "front"
+              ? "scale(1)"
+              : "scale(0.65)",
       }}
-    >{index}{content}</Box>
+    >
+      {index}
+      {content}
+    </Box>
   );
 };
 
